2e06efaeabc46e19e320720d29c1999b471eddfa
Filip <me@filipkin.com>
2025-08-03 01:10:53 +1200

    Remove robot shadow thing

--- packages/ui/src/screens/match-preview/MatchPreview.svelte
+++ packages/ui/src/screens/match-preview/MatchPreview.svelte
@@ -4,7 +4,6 @@
 	import { state } from "../../lib/state";
 	import { createEventDispatcher, onMount } from "svelte";
 	import { matchName } from "../../lib/matchNamer";
-	import RobotShadow from "./RobotShadow.svelte";
 	import { settings } from "../../lib/settings";
 	import TeamCard from "../../lib/TeamCard.svelte";
 
@@ -34,26 +33,6 @@
 
 	$: console.log($state.eventDetails);
 </script>
-
-<div class="w-full bg-primary-800 h-full fixed -skew-x-12 flex flex-row justify-end" style={`right: ${$shutterSpring}vw`}>
-	<div id="shadows" class="skew-x-12 flex flex-col w-1/2 items-center justify-center">
-		{#if $state.match}
-			{#each $state.match.teams[$settings.invert ? "red" : "blue"] as team}
-				<RobotShadow color={$settings.invert ? "red" : "blue"} teamNumber={team.number} />
-			{/each}
-		{/if}
-	</div>
-</div>
-
-<div class="w-full bg-primary-700 h-full fixed -skew-x-12 flex flex-row justify-start" style={`left: ${$shutterSpring}vw`}>
-	<div id="shadows" class="skew-x-12 flex flex-col w-1/2 items-center justify-center">
-		{#if $state.match}
-			{#each $state.match.teams[$settings.invert ? "blue" : "red"] as team}
-				<RobotShadow color={$settings.invert ? "blue" : "red"} teamNumber={team.number} />
-			{/each}
-		{/if}
-	</div>
-</div>
 
 <div class="fixed flex flex-col w-full h-full justify-around">
 	<div class="w-full flex flex-row justify-around py-16">
